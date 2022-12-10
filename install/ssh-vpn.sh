680834356398ae93309fbefb76bb7e3ab5381ecc
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-12-11 01:45:11 +0800

    Add files via upload

---
+++ install/ssh-vpn.sh
@@ -0,0 +1,367 @@
+#!/bin/bash
+# By pakyavpn
+# ==================================================
+#wget https://github.com/${GitUser}/
+GitUser="GH-reyz"
+red='\e[1;31m'
+green='\e[0;32m'
+purple='\e[0;35m'
+orange='\e[0;33m'
+NC='\e[0m'
+# initializing var
+export DEBIAN_FRONTEND=noninteractive
+MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+NET=$(ip -o $ANU -4 route show to default | awk '{print $5}');
+source /etc/os-release
+ver=$VERSION_ID
+
+#Detail
+country="MY"
+state="Kelantan"
+locality="Kota Bharu"
+organization="pakyavpn-Project"
+organizationalunit="pakyavpn-Project"
+commonname="pakyavpn-Project"
+email="pakyavpn-project@gmail.com"
+
+# go to root
+cd
+
+# Edit file /etc/systemd/system/rc-local.service
+cat > /etc/systemd/system/rc-local.service <<-END
+[Unit]
+Description=/etc/rc.local
+ConditionPathExists=/etc/rc.local
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+StandardOutput=tty
+RemainAfterExit=yes
+SysVStartPriority=99
+[Install]
+WantedBy=multi-user.target
+END
+
+# nano /etc/rc.local
+cat > /etc/rc.local <<-END
+#!/bin/sh -e
+# rc.local
+# By default this script does nothing.
+exit 0
+END
+
+# Ubah izin akses
+chmod +x /etc/rc.local
+
+# enable rc local
+systemctl enable rc-local
+systemctl start rc-local.service
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+#update
+apt update -y
+apt upgrade -y
+apt dist-upgrade -y
+apt-get remove --purge ufw firewalld -y
+apt-get remove --purge exim4 -y
+
+# install wget and curl
+apt -y install wget curl
+
+# install netfilter-persistent
+apt-get install netfilter-persistent
+
+# set time GMT +8
+ln -fs /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
+
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+
+# install
+apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
+echo "clear" >> .profile
+echo "menu" >> .profile
+
+# install webserver
+apt -y install nginx
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/GH-reyz/padu/main/system/nginx.conf"
+mkdir -p /home/vps/public_html
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/GH-reyz/padu/main/system/vps.conf"
+/etc/init.d/nginx restart
+
+# // install badvpn
+cd
+wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/${GitUser}/padu/main/badvpn-udpgw64"
+chmod +x /usr/bin/badvpn-udpgw
+sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500' /etc/rc.local
+sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500' /etc/rc.local
+sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500' /etc/rc.local
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7400 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7500 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7700 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7800 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900 --max-clients 500
+
+# // setting port ssh
+cd
+apt-get -y update
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g'
+
+# /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 500' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 40000' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 51443' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 58080' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 200' /etc/ssh/sshd_config
+sed -i 's/#Port 22/Port 22/g' /etc/ssh/sshd_config
+/etc/init.d/ssh restart
+
+# // install dropbear
+apt -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 109 -p 68 -p 69 -p 110"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+/etc/init.d/dropbear restart
+
+# // install squid for debian 9,10 & ubuntu 20.04
+apt -y install squid3
+
+# install squid for debian 11
+apt -y install squid
+wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/${GitUser}/padu/main/squid3.conf"
+sed -i $MYIP2 /etc/squid/squid.conf
+
+# setting vnstat
+apt -y install vnstat
+/etc/init.d/vnstat restart
+apt -y install libsqlite3-dev
+wget https://humdi.net/vnstat/vnstat-2.6.tar.gz
+tar zxvf vnstat-2.6.tar.gz
+cd vnstat-2.6
+./configure --prefix=/usr --sysconfdir=/etc && make && make install
+cd
+vnstat -u -i $NET
+sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
+chown vnstat:vnstat /var/lib/vnstat -R
+systemctl enable vnstat
+/etc/init.d/vnstat restart
+rm -f /root/vnstat-2.6.tar.gz
+rm -rf /root/vnstat-2.6
+
+# // install stunnel
+apt install stunnel4 -y
+cat > /etc/stunnel/stunnel.conf <<-END
+cert = /etc/stunnel/stunnel.pem
+client = no
+socket = a:SO_REUSEADDR=1
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+
+[dropbear]
+accept = 222
+connect = 127.0.0.1:22
+
+[dropbear]
+accept = 777
+connect = 127.0.0.1:109
+
+[openvpn]
+accept = 442
+connect = 127.0.0.1:1194
+
+[kontol-stunnel]
+accept = 2096
+connect = 127.0.0.1:2091
+END
+
+# // make a certificate
+openssl genrsa -out key.pem 2048
+openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
+-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+
+# // konfigurasi stunnel
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+/lib/systemd/systemd-sysv-install enable stunnel4
+systemctl start stunnel4
+/etc/init.d/stunnel4 restart
+
+# // OpenVPN
+wget https://raw.githubusercontent.com/${GitUser}/padu/main/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
+
+# install fail2ban
+apt -y install fail2ban
+
+# Instal DDOS Flate
+if [ -d '/usr/local/ddos' ]; then
+	echo; echo; echo "Please un-install the previous version first"
+	exit 0
+else
+	mkdir /usr/local/ddos
+fi
+clear
+echo; echo 'Installing DOS-Deflate 0.6'; echo
+echo; echo -n 'Downloading source files...'
+wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+echo -n '.'
+wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+echo -n '.'
+wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+echo -n '.'
+wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+chmod 0755 /usr/local/ddos/ddos.sh
+cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
+echo '...done'
+echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
+/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
+echo '.....done'
+echo; echo 'Installation has completed.'
+echo 'Config file is at /usr/local/ddos/ddos.conf'
+echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
+
+# banner /etc/issue.net
+wget -O /etc/issue.net "https://raw.githubusercontent.com/${GitUser}/padu/main/system/bannerssh.conf"
+echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
+
+# install lolcat
+wget https://raw.githubusercontent.com/${GitUser}/padu/main/system/lolcat.sh &&  chmod +x lolcat.sh && ./lolcat.sh
+
+#Bannerku menu
+wget -O /usr/bin/bannerku https://raw.githubusercontent.com/${GitUser}/padu/main/system/bannerku && chmod +x /usr/bin/bannerku
+
+#install bbr
+wget https://raw.githubusercontent.com/${GitUser}/padu/main/system/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+
+# blockir torrent
+iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
+iptables -A FORWARD -m string --string "announce_peer" --algo bm -j DROP
+iptables -A FORWARD -m string --string "find_node" --algo bm -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "BitTorrent protocol" -j DROP
+iptables -A FORWARD -m string --algo bm --string "peer_id=" -j DROP
+iptables -A FORWARD -m string --algo bm --string ".torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce.php?passkey=" -j DROP
+iptables -A FORWARD -m string --algo bm --string "torrent" -j DROP
+iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
+iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save
+netfilter-persistent reload
+
+# install resolvconf service
+apt install resolvconf -y
+
+#start resolvconf service
+systemctl start resolvconf.service
+systemctl enable resolvconf.service
+
+# download script
+cd /usr/bin
+wget -O menu-ssh "https://raw.githubusercontent.com/${GitUser}/padu/main/menu/menu-ssh.sh"
+wget -O add-ssh "https://raw.githubusercontent.com/${GitUser}/padu/main/add-user/add-ssh.sh"
+wget -O trial "https://raw.githubusercontent.com/${GitUser}/padu/main/add-user/trial.sh"
+wget -O del-ssh "https://raw.githubusercontent.com/${GitUser}/padu/main/add-user/del-ssh.sh"
+wget -O member "https://raw.githubusercontent.com/${GitUser}/padu/main/member.sh"
+wget -O delete "https://raw.githubusercontent.com/${GitUser}/padu/main/add-user/delete.sh"
+wget -O cek-ssh "https://raw.githubusercontent.com/${GitUser}/padu/main/add-user/cek-ssh.sh"
+wget -O add-host "https://raw.githubusercontent.com/${GitUser}/padu/main/system/add-host.sh"
+wget -O speedtest "https://raw.githubusercontent.com/${GitUser}/padu/main/system/speedtest_cli.py"
+wget -O xp "https://raw.githubusercontent.com/${GitUser}/padu/main/system/xp.sh"
+wget -O menu "https://raw.githubusercontent.com/${GitUser}/padu/main/menu/menu.sh"
+wget -O status "https://raw.githubusercontent.com/${GitUser}/padu/main/system/status.sh"
+wget -O info "https://raw.githubusercontent.com/${GitUser}/padu/main/system/info.sh"
+wget -O restart "https://raw.githubusercontent.com/${GitUser}/padu/main/system/restart.sh"
+wget -O ram "https://raw.githubusercontent.com/${GitUser}/padu/main/system/ram.sh"
+wget -O dns "https://raw.githubusercontent.com/${GitUser}/padu/main/system/dns.sh"
+wget -O nf "https://raw.githubusercontent.com/${GitUser}/padu/main/system/nf.sh"
+wget -O themes "https://raw.githubusercontent.com/${GitUser}/padu/main/menu/themes.sh"
+chmod +x menu-ssh
+chmod +x add-ssh
+chmod +x trial
+chmod +x del-ssh
+chmod +x member
+chmod +x delete
+chmod +x cek-ssh
+chmod +x menu
+chmod +x add-host
+chmod +x speedtest
+chmod +x xp
+chmod +x status
+chmod +x info
+chmod +x restart
+chmod +x ram
+chmod +x dns
+chmod +x nf
+chmod +x themes
+echo "0 0 * * * root /usr/bin/delete" >> /etc/crontab
+echo "0 6 * * * root reboot" >> /etc/crontab
+echo "0 0 * * * root /usr/bin/xp" >> /etc/crontab
+echo "*/2 * * * * root /usr/bin/cleaner" >> /etc/crontab
+cd
+
+service cron restart >/dev/null 2>&1
+service cron reload >/dev/null 2>&1
+
+# remove unnecessary files
+cd
+apt autoclean -y
+apt -y remove --purge unscd
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove bind9*;
+apt-get -y remove sendmail*
+apt autoremove -y
+# finishing
+cd
+chown -R www-data:www-data /home/vps/public_html
+sleep 1
+echo -e "[ ${green}ok${NC} ] Restarting nginx"
+/etc/init.d/nginx restart >/dev/null 2>&1
+sleep 1
+echo -e "[ ${green}ok${NC} ] Restarting openvpn"
+/etc/init.d/openvpn restart >/dev/null 2>&1
+sleep 1
+echo -e "[ ${green}ok${NC} ] Restarting cron "
+/etc/init.d/cron restart >/dev/null 2>&1
+sleep 1
+echo -e "[ ${green}ok${NC} ] Restarting ssh "
+/etc/init.d/ssh restart >/dev/null 2>&1
+sleep 1
+echo -e "[ ${green}ok${NC} ] Restarting dropbear "
+/etc/init.d/dropbear restart >/dev/null 2>&1
+sleep 1
+echo -e "[ ${green}ok${NC} ] Restarting fail2ban"
+/etc/init.d/fail2ban restart >/dev/null 2>&1
+sleep 1
+echo -e "[ ${green}ok${NC} ] Restarting resolvconf"
+/etc/init.d/resolvconf restart >/dev/null 2>&1
+sleep 1
+echo -e "[ ${green}ok${NC} ] Restarting vnstat"
+/etc/init.d/vnstat restart >/dev/null 2>&1
+echo -e "[ ${green}ok${NC} ] Restarting stunnel4"
+/etc/init.d/stunnel4 restart >/dev/null 2>&1
+echo -e "[ ${green}ok${NC} ] Restarting squid"
+/etc/init.d/squid restart >/dev/null 2>&1
+history -c
+echo "unset HISTFILE" >> /etc/profile
+
+cd
+rm -f /root/ssh-vpn.sh
+
+# finishing
+clear

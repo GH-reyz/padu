1fc6fc54bd35ae3a56886e9a75b5cc588ec59cba
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-12-11 01:38:35 +0800

    Add files via upload

---
+++ menu.sh
@@ -0,0 +1,256 @@
+#!/bin/bash
+#wget https://github.com/${GitUser}/
+clear
+GitUser="GH-reyz"
+MYIP=$(curl -sS ipv4.icanhazip.com)
+#Colour
+white='\e[0;37m'
+green='\e[0;32m'
+red='\e[0;31m'
+blue='\e[0;34m'
+cyan='\e[0;36m'
+yellow='\e[0;33m'
+NC='\e[0m'
+clear
+echo ""
+fun_bar () {
+comando[0]="$1"
+comando[1]="$2"
+ (
+[[ -e $HOME/fim ]] && rm $HOME/fim
+${comando[0]} -y > /dev/null 2>&1
+${comando[1]} -y > /dev/null 2>&1
+touch $HOME/fim
+ ) > /dev/null 2>&1 &
+ tput civis
+echo -ne "  \033[1;34mLoading \033[1;36m- \033[1;34m["
+while true; do
+   for((i=0; i<2; i++)); do
+   echo -ne "\033[1;36m#"
+   sleep 0.1s
+   done
+   [[ -e $HOME/fim ]] && rm $HOME/fim && break
+   echo -e "\033[1;34m]"
+   sleep 1s
+   tput cuu1
+   tput dl1
+   echo -ne "  \033[1;34mWait \033[1;34m- \033[1;34m["
+done
+echo -e "\033[1;34m]\033[1;36m -\033[1;32m OK !\033[1;37m"
+tput cnorm
+}
+clear
+domain=$(cat /usr/local/etc/xray/domain)
+# STATUS EXPIRED ACTIVE
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[4$below" && Font_color_suffix="\033[0m"
+Info="${Green_font_prefix}(Active)${Font_color_suffix}"
+Error="${Green_font_prefix}${Font_color_suffix}${Red_font_prefix}[EXPIRED]${Font_color_suffix}"
+
+# CERTIFICATE STATUS
+d1=$(date -d "$valid" +%s)
+d2=$(date -d "$today" +%s)
+certifacate=$(((d1 - d2) / 86400))
+version=$(cat /home/ver)
+ver=$( curl https://raw.githubusercontent.com/${GitUser}/version/main/version.conf )
+clear
+# CEK UPDATE
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info1="${Green_font_prefix}($version)${Font_color_suffix}"
+Info2="${Green_font_prefix}(LATEST VERSION)${Font_color_suffix}"
+Error="Version ${Green_font_prefix}[$ver]${Font_color_suffix} available${Red_font_prefix}[Please Update]${Font_color_suffix}"
+version=$(cat /home/ver)
+new_version=$( curl https://raw.githubusercontent.com/${GitUser}/version/main/version.conf | grep $version )
+#Status Version
+if [ $version = $new_version ]; then
+stl="${Info2}"
+else
+stl="${Error}"
+fi
+clear
+# USERNAME
+rm -f /usr/bin/user
+username=$(curl https://raw.githubusercontent.com/${GitUser}/allow1/main/ipvps.conf | grep $MYIP | awk '{print $2}')
+echo "$username" >/usr/bin/user
+# Order ID
+rm -f /usr/bin/ver
+user=$(curl https://raw.githubusercontent.com/${GitUser}/allow1/main/ipvps.conf | grep $MYIP | awk '{print $3}')
+echo "$user" >/usr/bin/ver
+# validity
+rm -f /usr/bin/e
+valid=$(curl https://raw.githubusercontent.com/${GitUser}/allow1/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+echo "$valid" >/usr/bin/e
+# DETAIL ORDER
+username=$(cat /usr/bin/user)
+oid=$(cat /usr/bin/ver)
+exp=$(cat /usr/bin/e)
+clear
+today=$(date -d "0 days" +"%Y-%m-%d")
+Exp1=$(curl https://raw.githubusercontent.com/GH-reyz/allow1/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+if [[ $today < $Exp1 ]]; then
+    sts="${Info}"
+else
+    sts="${Error}"
+fi
+echo -e "\e[32mloading...\e[0m"
+clear
+# PROVIDED
+creditt=$(cat /root/provided)
+# BANNER COLOUR
+banner_colour=$(cat /etc/banner)
+# TEXT ON BOX COLOUR
+box=$(cat /etc/box)
+# LINE COLOUR
+line=$(cat /etc/line)
+# TEXT COLOUR ON TOP
+text=$(cat /etc/text)
+# TEXT COLOUR BELOW
+below=$(cat /etc/below)
+# BACKGROUND TEXT COLOUR
+back_text=$(cat /etc/back)
+# NUMBER COLOUR
+number=$(cat /etc/number)
+# BANNER
+banner=$(cat /usr/bin/bannerku)
+ascii=$(cat /usr/bin/test)
+
+# // nginx status
+nginx=$( systemctl status nginx | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+if [[ $nginx == "running" ]]; then
+    status_nginx="${GREEN}ON${NC}"
+else
+    status_nginx="${RED}OFF${NC}"
+fi
+
+# // xray status
+xray=$( systemctl status xray | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+if [[ $xray == "running" ]]; then
+    status_xray="${GREEN}ON${NC}"
+else
+    status_xray="${RED}OFF${NC}"
+fi
+clear
+echo -e "\e[$banner_colour"
+figlet -f $ascii "$banner"
+echo -e "\e[$text Premium Script"
+echo -e "${blue}════════════════════════════════════════════════════════════${NC}"
+tput setaf 7 ; tput setab 6 ; tput bold ; printf '%45s%s%-15s\n' "WELCOME TO PREMIUM SCRIPT MENU" ; tput sgr0
+echo -e "${blue}════════════════════════════════════════════════════════════${NC}"
+echo -e "      ${cyan}Created By ${red}PAKYAVPN${NC} (${green}https://t.me/anakjati567${NC})"
+echo -e ""
+uphours=`uptime -p | awk '{print $2,$3}' | cut -d , -f1`
+upminutes=`uptime -p | awk '{print $4,$5}' | cut -d , -f1`
+uptimecek=`uptime -p | awk '{print $6,$7}' | cut -d , -f1`
+cekup=`uptime -p | grep -ow "day"`
+IPVPS=$(curl -s icanhazip.com/ip )
+if [ "$cekup" = "day" ]; then
+echo -e " ${white} System Uptime       : ${green} $uphours $upminutes $uptimecek"
+else
+echo -e " ${white} System Uptime       : ${green} $uphours $upminutes"
+fi
+echo -e " ${white} VPN Core            : ${green} XRAY-Core$NC"
+echo -e " ${white} Domain              : ${green} $domain$NC"
+echo -e " ${white} IP VPS              : ${green} $IPVPS$NC"
+echo -e " ${white} Order ID            : ${green} $oid$NC"
+echo -e " ${white} Provided By         : ${green} $creditt $NC"
+echo -e " ${white} Client Name         : ${green} $username $NC"
+echo -e " ${white} Expiry Script       : ${green} $exp $sts $NC"
+echo -e " ${white} Autoscript By       : ${green} PAKYAVPN $Info1 $NC"
+echo -e "${blue}════════════════════════════════════════════════════════════${NC}"
+echo -e "         ${cyan} [ XRAY-CORE${NC} : ${status_xray} ]          ${cyan} [ NGINX${NC} : ${status_nginx} ]"
+echo -e "${blue}════════════════════════════════════════════════════════════${NC}"
+echo -e "\\E[0;46;37m                        TUNNELING MENU                      \E[0m"
+echo -e "${blue}════════════════════════════════════════════════════════════${NC}"
+echo -e "  $green[${white}1${green}] ${green} XRAY Vmess WS Panel$NC"
+echo -e "  $green[${white}2${green}] ${green} XRAY Vless WS Panel$NC"
+echo -e "  $green[${white}3${green}] ${green} XRAY Trojan WS Panel$NC"
+echo -e "  $green[${white}4${green}] ${green} XRAY Vless TCP XTLS Panel$NC"
+echo -e "  $green[${white}5${green}] ${green} XRAY Trojan TCP Panel$NC"
+echo -e "  $green[${white}6${green}] ${green} Trojan GO Panel$NC"
+echo -e "${blue}════════════════════════════════════════════════════════════${NC}"
+echo -e "\\E[0;46;37m                        SYSTEM MENU                         \E[0m"
+echo -e "${blue}════════════════════════════════════════════════════════════${NC}"
+echo -e "  $green[${white}7${green}] ${green}Change Domain ${cyan}(add-host)$NC"
+echo -e "  $green[${white}8${green}] ${green}Menu Themes ${cyan}(themes)$NC"
+echo -e "  $green[${white}9${green}] ${green}Renew Certificate XRAY ${cyan}(cert)$NC"
+echo -e "  $green[${white}10${green}] ${green}Check VPN Statusn ${cyan}(status)$NC"
+echo -e "  $green[${white}11${green}] ${green}INFO All Port ${cyan}(info)$NC"
+echo -e "  $green[${white}12${green}] ${green}Restart VPN Servicesn${cyan}(restart)$NC"
+echo -e "  $green[${white}13${green}] ${green}Speedtest VPS ${cyan}(speedtest)$NC"
+echo -e "  $green[${white}14${green}] ${green}Check RAM ${cyan}(ram)$NC"
+echo -e "  $green[${white}15${green}] ${green}DNS Changer ${cyan}(dns)$NC"
+echo -e "  $green[${white}16${green}] ${green}Netflix Checker ${cyan}(nf)$NC"
+echo -e "  $green[${white}17${green}] ${green}Check Bandwith Server ${cyan}(vnstat)$NC"
+echo -e "  $green[${white}18${green}] ${green}Backup ${cyan}(backup)$NC"
+echo -e "  $green[${white}19${green}] ${green}Restore ${cyan}(restore)$NC"
+echo -e "${blue}════════════════════════════════════════════════════════════${NC}"
+echo -e "  $red[${white}0${red}] ${white}Exit from the menu ${cyan}(Ctrl+C)$NC"
+echo -e "${blue}════════════════════════════════════════════════════════════${NC}"
+read -p "  $(echo -e     ${white}Enter Your Options  ${green}[${NC}${white}1-20 or x${green}]${NC} :)  "  main
+echo -e ""
+case $main in
+1)
+menu-ws
+;;
+2)
+menu-vless
+;;
+3)
+menu-tr
+;;
+4)
+menu-xray
+;;
+5)
+menu-xtr
+;;
+6)
+menu-trgo
+;;
+7)
+add-host
+;;
+8)
+themes
+;;
+9)
+certxray
+;;
+10)
+status
+;;
+11)
+info
+;;
+12)
+restart
+;;
+13)
+speedtest
+;;
+14)
+ram
+;;
+15)
+dns
+;;
+16)
+nf
+;;
+17)
+vnstat
+;;
+18)
+backup
+;;
+19)
+restore
+;;
+0)
+exit
+;;
+*)
+echo -e "${red}Please enter an correct number ${NC}"
+sleep 1
+menu
+;;
+esac
---
+++ setup.sh
@@ -0,0 +1,260 @@
+#!/bin/bash
+#wget https://github.com/${GitUser}/
+GitUser="GH-reyz"
+# Color Validation
+Lred='\e[1;91m'
+Lgreen='\e[92m'
+Lyellow='\e[93m'
+green='\e[32m'
+RED='\033[0;31m'
+NC='\033[0m'
+BGBLUE='\e[1;44m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+purple='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0;37m'
+# ===================
+echo ''
+clear
+echo ''
+echo -e "$Lyellow                ⚡ PREMIUM SPEED SCRIPT ⚡"$NC
+echo -e "$green.........................................................."$NC
+echo -e "$Lyellow                  Autoscript By ReyzVPN"$NC
+echo -e "$Lyellow                    CONTACT TELEGRAM"$NC
+echo -e "$Lyellow                       @GHReyz"$NC
+echo -e "$Lyellow                       @Reyz-V4"$NC
+echo -e "$green.........................................................."$NC
+echo ''
+echo -e "$Lyellow                       Tunggu 6 Saat!"$NC
+echo -e "$green.........................................................."$NC
+sleep 6
+clear
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+clear
+# Valid Script
+VALIDITY() {
+    today=$(date -d "0 days" +"%Y-%m-%d")
+    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow1/main/ipvps.conf | grep $MYIP | awk '{print $4}')
+    if [[ $today < $Exp1 ]]; then
+        echo -e "\e[32mTahniah! Anda Dibenarkan menggunakan AUTOSCRIPT PAKYAVPN..\e[0m"
+        sleep 5
+    else
+        echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m"
+        echo -e "\e[31mPlease renew your ipvps first\e[0m"
+        exit 0
+    fi
+}
+IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow1/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+    echo -e "\e[32mPermission Accepted...\e[0m"
+    VALIDITY
+else
+    echo -e "\e[31mPermission Denied!\e[0m"
+    echo -e "\e[31mPlease buy script first\e[0m"
+    rm -f setup.sh
+    exit 0
+fi
+clear
+secs_to_human() {
+    echo "Installation time : $(( ${1} / 3600 )) hours $(( (${1} / 60) % 60 )) minutes $(( ${1} % 60 )) seconds"
+}
+start=$(date +%s)
+if [ -f "/usr/local/etc/xray/domain" ]; then
+echo "Script Already Installed"
+exit 0
+fi
+mkdir /var/lib/premium-script;
+mkdir /var/lib/crot-script;
+clear
+clear
+#Nama penyedia script
+echo -e "\e[1;32m════════════════════════════════════════════════════════════\e[0m"
+echo ""
+echo -e "   \e[1;32mPlease enter the name of Provider for Script."
+read -p "   Client Name : " nm
+echo $nm > /root/provided
+echo ""
+echo -e "\e[1;32m════════════════════════════════════════════════════════════\e[0m"
+echo ""
+echo -e "   .----------------------------------."
+echo -e "   |\e[1;32mPlease select a domain type below \e[0m|"
+echo -e "   '----------------------------------'"
+echo -e "     \e[1;32m1)\e[0m Enter your Subdomain"
+echo -e "     \e[1;32m2)\e[0m Use A Random Subdomain"
+echo -e "   ------------------------------------"
+read -p "   Please select numbers 1-2 or Any Button(Random) : " padu
+echo ""
+if [[ $padu == "1" ]]; then
+echo -e "   \e[1;32mPlease enter your subdomain "
+read -p "   Subdomain: " host
+echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
+echo "IP=$host" >> /var/lib/crot-script/ipvps.conf
+echo "$host" >> /root/domain
+echo ""
+elif [[ $padu == "2" ]]; then
+#install cf
+wget https://raw.githubusercontent.com/${GitUser}/padu/main/install/cf.sh && chmod +x cf.sh && ./cf.sh
+rm -f /root/cf.sh
+clear
+else
+echo -e "Random Subdomain/Domain is used"
+wget https://raw.githubusercontent.com/${GitUser}/padu/main/install/cf.sh && chmod +x cf.sh && ./cf.sh
+rm -f /root/cf.sh
+clear
+fi
+echo ""
+clear
+echo -e "\e[0;32mREADY FOR INSTALLATION SCRIPT...\e[0m"
+echo -e ""
+sleep 1
+#Install SSH-VPN
+echo -e "\e[0;32mINSTALLING SSH-VPN...\e[0m"
+sleep 1
+wget https://raw.githubusercontent.com/${GitUser}/padu/main/install/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+echo -e "\e[0;32mDONE INSTALLING SSH-VPN\e[0m"
+echo -e ""
+sleep 1
+#Install Xray
+echo -e "\e[0;32mINSTALLING XRAY CORE...\e[0m"
+sleep 1
+wget https://raw.githubusercontent.com/${GitUser}/padu/main/install/ins-xray.sh && chmod +x ins-xray.sh && screen -S ins-xray ./ins-xray.sh
+echo -e "\e[0;32mDONE INSTALLING XRAY CORE\e[0m"
+echo -e ""
+sleep 1
+clear
+#Install SET-BR
+echo -e "\e[0;32mINSTALLING SET-BR...\e[0m"
+sleep 1
+wget https://raw.githubusercontent.com/${GitUser}/padu/main/install/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+echo -e "\e[0;32mDONE INSTALLING SET-BR...\e[0m"
+echo -e ""
+sleep 1
+clear
+# set time GMT +8
+ln -fs /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
+# install clouflare JQ
+apt install jq curl -y
+
+#finish
+rm -f /root/ins-xray.sh
+rm -f /root/set-br.sh
+rm -f /root/ssh-vpn.sh
+
+# move client name to xray folder
+mv /root/user /usr/local/etc/xray/
+
+# Colour Default
+echo "1;36m" > /etc/banner
+echo "30m" > /etc/box
+echo "1;31m" > /etc/line
+echo "1;32m" > /etc/text
+echo "1;33m" > /etc/below
+echo "47m" > /etc/back
+echo "1;35m" > /etc/number
+echo 3d > /usr/bin/test
+# Version
+ver=$( curl https://raw.githubusercontent.com/${GitUser}/version/main/version.conf )
+history -c
+echo "$ver" > /home/ver
+clear
+echo ""
+echo ""
+echo -e "    .-------------------------------------------."
+echo -e "    |      Installation Has Been Completed      |"
+echo -e "    '-------------------------------------------'"
+echo ""
+echo ""
+echo -e "\e[1;32m═════════════════════[-Autoscript-Premium-]═════════════════════${NC}" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    >>> Service Details <<<"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    [INFORMASI SSH & OpenVPN]" | tee -a log-install.txt
+echo -e "${purple}   --------------------${NC}" | tee -a log-install.txt
+echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
+echo "   - OpenVPN                 : TCP 1194, UDP 2200"  | tee -a log-install.txt
+echo "   - OpenVPN SSL             : 110"  | tee -a log-install.txt
+echo "   - Stunnel4                : 222, 777"  | tee -a log-install.txt
+echo "   - Dropbear                : 143, 109"  | tee -a log-install.txt
+echo "   - Websocket SSH(HTTP)     : 80"  | tee -a log-install.txt
+echo "   - Websocket SSL(HTTPS)    : 443, 2096"  | tee -a log-install.txt
+echo "   - Websocket OpenVPN       : 2097"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    [INFORMASI Sqd, Bdvp, Ngnx]" | tee -a log-install.txt
+echo "    ---------------------------" | tee -a log-install.txt
+echo "   - Squid Proxy             : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
+echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
+echo "   - Nginx                   : 81"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   [ XRAY INFORMATION ]" | tee -a log-install.txt
+echo -e "${purple}   --------------------${NC}" | tee -a log-install.txt
+echo "   - XRAY VMESS WS TLS       : 443"  | tee -a log-install.txt
+echo "   - XRAY VLESS WS TLS       : 443"  | tee -a log-install.txt
+echo "   - XRAY VLESS TCP XTLS     : 443"  | tee -a log-install.txt
+echo "   - XRAY VMESS WS None TLS  : 80"  | tee -a log-install.txt
+echo "   - XRAY VLESS WS None TLS  : 80"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "    [INFORMASI TROJAN]"  | tee -a log-install.txt
+echo -e "${purple}   --------------------${NC}" | tee -a log-install.txt
+echo "   - XRAY TROJAN TCP TLS     : 443"  | tee -a log-install.txt
+echo "   - XRAY TROJAN WS TLS      : 443"  | tee -a log-install.txt
+echo "   - XRAY TROJAN WS None TLS : 80"  | tee -a log-install.txt
+echo "   - TROJAN GO WS            : 8000"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   [ YAML INFORMATION ]" | tee -a log-install.txt
+echo -e "${purple}   --------------------${NC}" | tee -a log-install.txt
+echo "   - YAML XRAY VMESS WS"  | tee -a log-install.txt
+echo "   - YAML XRAY VLESS WS"  | tee -a log-install.txt
+echo "   - YAML XRAY TROJAN WS"  | tee -a log-install.txt
+echo "   - YAML XRAY VLESS XTLS"  | tee -a log-install.txt
+echo "   - YAML XRAY TROJAN TCP"  | tee -a log-install.txt
+echo "   - YAML TROJAN GO"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   [ SERVER INFORMATION ]"  | tee -a log-install.txt
+echo -e "${purple}   ----------------------${NC}" | tee -a log-install.txt
+echo "   - Timezone                : Asia/Kuala_Lumpur (GMT +8)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPV6                    : [OFF]"  | tee -a log-install.txt
+echo "   - Autoreboot On 06.00 GMT +8" | tee -a log-install.txt
+echo "   - Autobackup Data" | tee -a log-install.txt
+echo "   - Restore Data" | tee -a log-install.txt
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo "   - White Label" | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   [ DEV INFORMATION ]" | tee -a log-install.txt
+echo -e "${purple}   -------------------${NC}" | tee -a log-install.txt
+echo "   - Autoscript Lite By      : ReyzVPN"  | tee -a log-install.txt
+echo "   - Telegram                : t.me/GHReyz"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo -e "\e[1;32m════════════════Autoscript By ReyzVPN════════════════${NC}" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+secs_to_human "$(($(date +%s) - ${start}))" | tee -a log-install.txt
+sleep 7
+clear
+echo ""
+echo -e "    \e[1;32m.------------------------------------------.\e[0m"
+echo -e "    \e[1;32m|     SUCCESFULLY INSTALLED THE SCRIPT     |\e[0m"
+echo -e "    \e[1;32m|         PREMIUM BY @GHReyz / @ReyzV4          |\e[0m"
+echo -e "    \e[1;32m'------------------------------------------'\e[0m"
+echo ""
+echo -e "   \e[1;32mYour VPS Will Be Automatical Reboot In 5 seconds\e[0m"
+rm -r setup.sh
+sleep 5
+reboot

eed89b6b1f121a9c76513ec7711475364ee84732
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-12-11 17:29:11 +0800

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -129,9 +129,9 @@ else
     status_xray="${RED}OFF${NC}"
 fi
 clear
-echo -e "\033[1;32m     ╔═╗┬─┐┌─┐┌┬┐┬┬ ┬┌┬┐  ╔═╗┌─┐┬─┐┬┌─┐┌┬┐" 
-echo -e "\033[1;32m     ╠═╝├┬┘├┤ │││││ ││││  ╚═╗│  ├┬┘│├─┘ │" 
-echo -e "\033[1;32m     ╩  ┴└─└─┘┴ ┴┴└─┘┴ ┴  ╚═╝└─┘┴└─┴┴   ┴ "                                                                                                        
+echo -e "\033[1;32m         ╔═╗┬─┐┌─┐┌┬┐┬┬ ┬┌┬┐  ╔═╗┌─┐┬─┐┬┌─┐┌┬┐" 
+echo -e "\033[1;32m         ╠═╝├┬┘├┤ │││││ ││││  ╚═╗│  ├┬┘│├─┘ │" 
+echo -e "\033[1;32m         ╩  ┴└─└─┘┴ ┴┴└─┘┴ ┴  ╚═╝└─┘┴└─┴┴   ┴ "                                                                                                        
 echo -e "\e[$text Premium Script"
 echo -e "${blue}════════════════════════════════════════════════════════════${NC}"
 tput setaf 7 ; tput setab 6 ; tput bold ; printf '%45s%s%-15s\n' "WELCOME TO PREMIUM SCRIPT " ; tput sgr0
